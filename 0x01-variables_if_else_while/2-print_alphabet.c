83acd8847005927a2cfe53c106b02b1d6d3f34e7
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-02-04 15:59:16 +0100

    re

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * print message
+ * Return: success
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char y;
+for (y = 'a'; y <= 'z' ; y++)
+{
+putchar (y);
+}
+	putchar('\n');
+
+	return (0);
+}
