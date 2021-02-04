c0c0dfe562f27a6fcdc6a6bcd8cda492121abda3
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-02-04 17:12:14 +0100

    print alpha z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * print message
+ * Return: 0
+ */
+
+int main(void)
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; --i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
