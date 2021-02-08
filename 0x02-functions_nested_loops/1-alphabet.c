96b9dc182e7f15112282c176bcc15ec78013a7f9
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-02-08 14:17:38 +0100

    x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*print_alphabet - 0
+*
+*
+*Return: 0
+*/
+void print_alphabet(void)
+{
+for (char c = 'a'; c <= 'z'; ++c)
+{
+_putchar("%c ", c);
+}
+_putchar("\n");
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,2 @@
+void print_alphabet_x10(void);
+int _putchar(char);
