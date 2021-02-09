75d200c723cbb5ceb3baf895c154aaee5fd7292d
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-02-09 15:10:08 +0100

    r

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,16 +1,12 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * add - add 2 integer.
+ *@a:integer.
+ *@b:integer.
  * Return: Always 0.
  */
-int main(void)
+int add(int a, int b)
 {
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+	return (a + b);
 }
