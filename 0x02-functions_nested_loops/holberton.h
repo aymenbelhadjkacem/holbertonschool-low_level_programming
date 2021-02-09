2028ea90e129c67c88035f59e76e7871352313cb
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-02-09 10:02:51 +0100

    r

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,12 @@
+#include <unistd.h>
+
+/**
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void print_alphabet_x10(void);
 int _putchar(char);
 int _islower(int c);
+void print_alphabet(void);
