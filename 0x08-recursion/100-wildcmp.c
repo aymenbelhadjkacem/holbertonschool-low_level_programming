6f658da2881b4db06d648e0b209842f92a092e55
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-02-24 12:46:34 +0100

    r

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -17,7 +17,7 @@ if (*s1 == *s2)
 return (wildcmp((s1 + 1), (s2 + 1)));
 if (*s2 == '*')
 {
-	if (*(s2 + 1) == '*')
+if (*(s2 + 1) == '*')
 return (wildcmp(s1, (s2 + 1)));
 else if (wildcmp(s1, s2 + 1))
 return (1);
