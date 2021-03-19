6e8fcfd780ee000e82ff38efc8ce717e7ff8106a
Brandixitor <med.salah.bourouba@gmail.com>
2021-03-19 11:04:14 +0100

    r

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+
+section .data
+fmt db "%s", 10, 0
+msg db "Hello, Holberton", 0
+
+section .text
+extern printf
+global main
+
+main:
+push	rbp
+
+mov	rdi,fmt
+mov	rsi,msg
+mov	rax,0
+call	printf
+
+pop	rbp
+mov	rax,0
+ret
